1c10bef83f4f5fd92977e2163091f267b560ca5f
Saul Vega <saulvega@Sauls-MacBook-Pro.local>
2022-09-14 00:25:01 -0600

    added 'gulp-clean-css' to minifiy css in live theme file

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 const gulp = require("gulp");
 const themeKit = require("@Shopify/themekit");
 const sass = require("gulp-sass")(require("sass"));
+const clean = require("gulp-clean-css");
 
 gulp.task("sass", function () {
-	return gulp.src("styles/custom-styles.scss").pipe(sass()).pipe(gulp.dest("assets"));
+	return gulp
+		.src("styles/custom-styles.scss")
+		.pipe(sass())
+		.pipe(clean({ compatibility: "ie11" }))
+		.pipe(gulp.dest("assets"));
 });
 
 gulp.task("watch", function () {
