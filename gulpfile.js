a0531fce00b1aef3d0dfcc18a98966abca7ffdb6
Saul Vega <saulvega@Sauls-MacBook-Pro.local>
2022-09-14 00:17:36 -0600

    added allow live in watch task in gulpfile.js to allow theme watch on live themes

--- gulpfile.js
+++ gulpfile.js
@@ -5,3 +5,11 @@ const sass = require("gulp-sass")(require("sass"));
 gulp.task("sass", function () {
 	return gulp.src("styles/custom-styles.scss").pipe(sass()).pipe(gulp.dest("assets"));
 });
+
+gulp.task("watch", function () {
+	gulp.watch("styles/**/*.scss", gulp.series("sass"));
+	themeKit.command("watch", {
+		env: "development",
+		allowLive: true,
+	});
+});
